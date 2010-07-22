6628fddf9046f10f6269d8eb3f0f83f6f4bb28b7
jpeterson <jpeterson@crossway.org>
2010-07-21 20:21:27 -0500

    Changing logic to use a while loop rather than the block.

--- lib/business_hours.rb
+++ lib/business_hours.rb
@@ -13,18 +13,15 @@ class BusinessHours
   end
 
   def calculate_deadline(remaining, start_time = Time.now)
-    Deadline.new(@schedule, remaining, start_time).calculate
+    Deadline.new(@schedule, remaining, Time.parse(start_time)).calculate
   end
 
   class Schedule
     DAYS = [:sun, :mon, :tue, :wed, :thur, :fri, :sat]
+
     def initialize(hours)
       @schedule = {}
       DAYS.each{|day| self[day]= hours}
-    end
-
-    def key(date)
-      (date.is_a? Symbol) ? date : Time.parse(date.to_s).strftime("%x")
     end
 
     def []=(date, value)
@@ -34,33 +31,33 @@ class BusinessHours
     def [](date)
       @schedule[key(date)] || @schedule[DAYS[Time.parse(date.to_s).wday]]
     end
+
+    private
+    def key(date)
+      (date.is_a? Symbol) ? date : Time.parse(date.to_s).strftime("%x")
+    end
   end
 
   class Deadline
-    def initialize(schedule, remaining, start_time)
-      @schedule, @remaining = schedule, remaining
-      @current_time = Time.parse(start_time)
+    def initialize(*args)
+      @schedule, @remaining, @current_time = *args
     end
 
     def calculate
-      each_day do
-        return @current_time + @remaining if ((@current_time + @remaining) <= @hours.last)
-        @remaining -= (@hours.last - @current_time) if ((@current_time <= @hours.last))
-      end
+      next_day while time_remaining?
+      return @current_time + @remaining
     end
 
-    def update_hours
+    private
+    def next_day
+      @remaining -= (@hours.last - @current_time)# adjust remaining hours
+      @current_time = Time.parse("0:00 AM", @current_time + 24*60*60)# increment to next day
+    end
+
+    def time_remaining?
       @hours = @schedule[@current_time].collect {|hour| Time.parse(hour, @current_time)}
-      @current_time = @hours.first if(@hours.first && @current_time < @hours.first)# adjust to starting time
-    end
-
-    # Iterate Scheduled Business Hours
-    def each_day
-      loop do
-        update_hours
-        yield
-        @current_time = Time.parse("0:00 AM", @current_time + 24*60*60)# prepare for next day
-      end
+      @current_time = (@current_time < @hours.first) ? @hours.first : (@current_time > @hours.last) ? @hours.last : @current_time # force to be inside normal business hours
+      (@current_time + @remaining) > @hours.last
     end
   end
 end
